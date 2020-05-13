c735ae740a034f4a1f5e0d590623761622c83568
Moses <lazaromoses77@gmail.com>
2020-05-14 01:37:06 +0300

    Updates

---
+++ precache-manifest.a308a71b6af57a7ffe05ebe7302eb60b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "839a115d0327d70e7035b709540b32c2",
+    "url": "/Map_2/index.html"
+  },
+  {
+    "revision": "c08d6f744723f84fe297",
+    "url": "/Map_2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e79de4117da3494625ae",
+    "url": "/Map_2/static/js/2.b1a93dce.chunk.js"
+  },
+  {
+    "revision": "2c598234c7a24970b5fdf032f602e7bc",
+    "url": "/Map_2/static/js/2.b1a93dce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c08d6f744723f84fe297",
+    "url": "/Map_2/static/js/main.3002b096.chunk.js"
+  },
+  {
+    "revision": "665bd7dd95b7ae8d6373",
+    "url": "/Map_2/static/js/runtime-main.915fa853.js"
+  }
+]);
